8e3a64d3295bc8bc709dee1ce17504f93c56d86e
GreeshmaKoneru <gkoneru@gmu.edu>
2025-11-06 18:13:12 -0500

    comments

--- converter.js
+++ converter.js
@@ -7,6 +7,6 @@ function toKelvin(celsius) {
   return celsius + 273.15;
 }
 
-// Output as per given
+// Output
 console.log(toFahrenheit(25)); // Output: 77°F
 console.log(toKelvin(25));     // 298.15K
